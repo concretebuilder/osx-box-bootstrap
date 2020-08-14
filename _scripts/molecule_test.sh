5719f1cc78881594e2d1110adc244950378ded18
fehersanyi-bitrise <sandor.feher@bitrise.io>
2020-08-14 10:08:35 +0200

    custom test configuration

--- _scripts/molecule_test.sh
+++ _scripts/molecule_test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 
-molecule test -s "${ROLE}" | tee "$BITRISE_DEPLOY_DIR/molecule.log"
+function run_test {
+    molecule destroy -s "$1"
+    molecule create -s "$1"
+    molecule converge -s "$1"
+    molecule verify -s "$1"
+    molecule destroy -s "$1"
+}
+
+run_test "${ROLE}" | tee "$BITRISE_DEPLOY_DIR/molecule.log"
