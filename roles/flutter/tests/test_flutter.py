055d113496e5888de7c7c9ec54248ecebd739066
Laszlo Meszaros <laszlo.meszaros@bitrise.io>
2020-08-25 10:42:48 +0200

    install flutter only if the given version is not already installed, fixed test

--- roles/flutter/tests/test_flutter.py
+++ roles/flutter/tests/test_flutter.py
@@ -1,3 +1,2 @@
 def test_flutter_properly_installed(host):
-    assert host.exists("flutter")
-    assert host.run("flutter --version").stdout.contains('1.20.2')
+    assert "Flutter 1.20.2" in host.run("/usr/local/flutter/bin/flutter --version").stdout
