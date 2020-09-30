963827da2b7579b3a6c97d074c10727adf61b2e9
Laszlo Meszaros <laszlo.meszaros@bitrise.io>
2020-09-30 12:44:47 +0200

    [IN-2032] use local clones as reote and not github

--- roles/brew/files/setup_brew_cask.sh
+++ roles/brew/files/setup_brew_cask.sh
@@ -6,4 +6,6 @@ source ~/.bash_profile
 
 brew_repository=$(brew --repository)
 
-git clone https://github.com/bitrise-io/homebrew-cask "${brew_repository}/Library/Taps/homebrew/homebrew-cask"
+git clone --bare https://github.com/bitrise-io/homebrew-cask /var/tmp/homebrew-cask
+
+git clone /var/tmp/homebrew-cask "${brew_repository}/Library/Taps/homebrew/homebrew-cask"
--- roles/brew/files/setup_brew_core.sh
+++ roles/brew/files/setup_brew_core.sh
@@ -8,6 +8,8 @@ brew_repository=$(brew --repository)
 
 rm -rf "$brew_repository/Library/Taps/homebrew"
 
-export HOMEBREW_CORE_GIT_REMOTE="https://github.com/bitrise-io/homebrew-core"
+git clone --bare https://github.com/bitrise-io/homebrew-core /var/tmp/homebrew-core
+
+export HOMEBREW_CORE_GIT_REMOTE="/var/tmp/homebrew-core"
 
 brew install a || true
