d3137152fe58f6a59a9c688c691ca9f94a9ba8a5
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-02-17 18:49:29 +0100

    only trigger a test if it is available tho

--- _scripts/check_changes.sh
+++ _scripts/check_changes.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 
 file_list="$(git diff --name-only origin/master.."$BITRISE_GIT_BRANCH" | uniq)"
 ansible_roles="$(ls ./roles)"
+molecule_contents="$(ls ./roles/tests/molecule)"
+# shellcheck disable=SC2207
+available_tests=($(comm -12 <(printf '%s\n' "${ansible_roles[@]}" | LC_ALL=C sort) <(printf '%s\n' "${molecule_contents[@]}" | LC_ALL=C sort)))
 
 echo "Files changed: ${file_list}"
 
@@ -12,14 +15,15 @@ number_of_changed_roles="$(git diff --name-only origin/master.."$BITRISE_GIT_BRA
 if [[ ${number_of_changed_roles} -gt 1 ]]; then
   echo "Please don't change multiple roles in one PR - Thanks, Build-Infra team"
   envman add --key DANGER_OUTPUT --value "Please don't change multiple roles in one PR - Thanks, Build-Infra team"
+
 elif [[ $file_list == *"roles"* ]]; then
-  echo "You changed a role, we will run molecule tests."
-  envman add --key DANGER_OUTPUT --value "You changed a role, we will run molecule tests."
-  envman add --key ROLE_CHANGED --value true
-  for role in ${ansible_roles}; do
+  echo "You changed a role, we will attempt to run molecule tests."
+  envman add --key DANGER_OUTPUT --value "You changed a role, we will attempt to run molecule tests."
+  for role in "${available_tests[@]}"; do
     if [[ $file_list == *$role* ]]; then
       echo "WORKFLOW_TO_TRIGGER: test_${role}"
       envman add --key WORKFLOW_TO_TRIGGER --value "test_${role}"
+      envman add --key ROLE_CHANGED --value true
     fi
   done
 else
