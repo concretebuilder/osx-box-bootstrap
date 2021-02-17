62564ed356ce34dae4b4c82de08a44ef12e44762
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-02-17 20:17:47 +0100

    remove debug logs from script

--- _scripts/check_changes.sh
+++ _scripts/check_changes.sh
@@ -9,22 +9,16 @@ molecule_contents="$(ls ./roles/tests/molecule)"
 available_tests=$(comm -12 <(printf '%s\n' "${ansible_roles[@]}" | LC_ALL=C sort) <(printf '%s\n' "${molecule_contents[@]}" | LC_ALL=C sort))
 number_of_changed_roles="$(git diff --name-only origin/master.."$BITRISE_GIT_BRANCH" | grep "roles" | cut -f1-2 -d"/" | uniq | grep -c roles)" || true # Do not exit with 1 if we don't change anything under roles
 
-echo "Files changed: ${file_list}"
-
 if [[ ${number_of_changed_roles} -gt 1 ]]; then
-  echo "Please don't change multiple roles in one PR - Thanks, Build-Infra team"
   envman add --key DANGER_OUTPUT --value "Please don't change multiple roles in one PR - Thanks, Build-Infra team"
 elif [[ $file_list == *"roles"* ]]; then
-  echo "You changed a role, we will attempt to run molecule tests."
   envman add --key DANGER_OUTPUT --value "You changed a role, we will attempt to run molecule tests."
   for role in "${available_tests[@]}"; do
     if [[ $file_list == *$role* ]]; then
-      echo "WORKFLOW_TO_TRIGGER: test_${role}"
       envman add --key WORKFLOW_TO_TRIGGER --value "test_${role}"
       envman add --key ROLE_CHANGED --value true
     fi
   done
 else
-  echo "No changes in ansible"
   envman add --key DANGER_OUTPUT --value "No changes in ansible"
 fi
