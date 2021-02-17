98b037358436ab563dd1f45665a698d98e8c9e50
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-02-17 16:49:34 +0100

    make ssh-setup change

--- _scripts/check_changes.sh
+++ _scripts/check_changes.sh
@@ -4,31 +4,17 @@ set -euo pipefail
 ## Check what changed in the current commit/PR
 
 file_list="$(git diff --name-only origin/master.."$BITRISE_GIT_BRANCH" | uniq)"
-number_of_changed_roles="$(git diff --name-only origin/master.."$BITRISE_GIT_BRANCH" | grep "terraform/dc/" | cut -d/ -f3 | uniq | grep -c macstadium)" || true # Do not exit with 1 if we don't change anything under terraform/dc
-
-echo "Files changed: ${file_list}"
-echo "Number of roless changed: ${number_of_changed_roles}"
-
-envman add --key CHANGED_TERRAFORM_CODE --value false
-envman add --key MODULES_CHANGE --value false
-envman add --key IS_ATL_PRIV --value false
-envman add --key IS_VEG_PUB --value false
-envman add --key IS_ATL3_PUB --value false
-envman add --key IS_LAS1_PRV1 --value false
-envman add --key IS_ATL01_PRV001 --value false
-
 ansible_roles="$(ls ./roles)"
 
-if [[ ${number_of_changed_roles} -gt 1 ]]; then
-  echo "You should not change multiple roles at the same time"
-  envman add --key DANGER_OUTPUT --value "You should not change multiple roles at the same time"
-elif [[ $file_list == *"roles"* ]]; then
+echo "Files changed: ${file_list}"
+
+if [[ $file_list == *"roles"* ]]; then
   echo "You changed a role, we will run molecule tests."
   envman add --key DANGER_OUTPUT --value "You changed a role, we will run molecule tests."
   for role in ${ansible_roles}; do
     if [[ $file_list == *$role* ]]; then
-      echo "Changed role: ${role}"
-      # envman add --key "${role}" --value true
+      echo "WORKFLOW_TO_TRIGGER: test_${role}"
+      envman add --key WORKFLOW_TO_TRIGGER --value "test_${role}"
     fi
   done
 else
