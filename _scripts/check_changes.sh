377e60279e8dd0dd22d1feb82d4302fc4649f68a
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-02-17 20:16:11 +0100

    fix formatting

--- _scripts/check_changes.sh
+++ _scripts/check_changes.sh
@@ -7,14 +7,13 @@ file_list="$(git diff --name-only origin/master.."$BITRISE_GIT_BRANCH" | uniq)"
 ansible_roles="$(ls ./roles)"
 molecule_contents="$(ls ./roles/tests/molecule)"
 available_tests=$(comm -12 <(printf '%s\n' "${ansible_roles[@]}" | LC_ALL=C sort) <(printf '%s\n' "${molecule_contents[@]}" | LC_ALL=C sort))
+number_of_changed_roles="$(git diff --name-only origin/master.."$BITRISE_GIT_BRANCH" | grep "roles" | cut -f1-2 -d"/" | uniq | grep -c roles)" || true # Do not exit with 1 if we don't change anything under roles
 
 echo "Files changed: ${file_list}"
 
-number_of_changed_roles="$(git diff --name-only origin/master.."$BITRISE_GIT_BRANCH" | grep "roles" | cut -f1-2 -d"/" | uniq | grep -c roles)" || true # Do not exit with 1 if we don't change anything under roles
 if [[ ${number_of_changed_roles} -gt 1 ]]; then
   echo "Please don't change multiple roles in one PR - Thanks, Build-Infra team"
   envman add --key DANGER_OUTPUT --value "Please don't change multiple roles in one PR - Thanks, Build-Infra team"
-
 elif [[ $file_list == *"roles"* ]]; then
   echo "You changed a role, we will attempt to run molecule tests."
   envman add --key DANGER_OUTPUT --value "You changed a role, we will attempt to run molecule tests."
