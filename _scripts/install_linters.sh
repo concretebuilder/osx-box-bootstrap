90133a5440fe782b7cac2cb2701354e771c202bf
Péter Héja <66314169+peterheja-bitrise@users.noreply.github.com>
2021-02-18 10:09:33 +0100

    [IN-2479]: use venv for CI

--- _scripts/install_linters.sh
+++ _scripts/install_linters.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
 set -e
-sudo pip3 install "ansible-lint==${ANSIBLE_LINT_VER}" --force-reinstall
-sudo pip3 install "yamllint==${YAML_LINT_VER}" --force-reinstall
+
+# If .venv dir does not exist, create one
+if [ !  -d "${PWD}/.venv" ]
+then
+    mkdir .venv
+fi
+
+# If virtual env does not exist, create one
+if [ ! -f "${PWD}/.venv/bin/ativate " ]
+then
+    python3 -m venv "${PWD}/.venv"
+fi
+
+# Activate virtual env and add it to PATH, so we use pip3 from there
+source "${PWD}/.venv/bin/activate"
+export PATH="${PWD}/.venv/bin/:${PATH}"
+
+# Set sane default versions
+export ANSIBLE_LINT_VER="${ANSIBLE_LINT_VER:-4.2.0}"
+export YAML_LINT_VER="${YAML_LINT_VER:-1.19.0}"
+
+# Install in virtual env
+pip3 install "ansible-lint==${ANSIBLE_LINT_VER}"
+pip3 install "yamllint==${YAML_LINT_VER}"
 
 yamllint --version
 ansible-lint --version
