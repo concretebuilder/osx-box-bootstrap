4eace3bc1373e74dd81880f844f0e191ac7b8005
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-03-17 09:19:21 +0100

    try with old path

--- roles/go/tests/test_go.py
+++ roles/go/tests/test_go.py
@@ -1,7 +1,7 @@
 # Go tests
 def test_if_go_exists(host):
-    assert host.exists("go")
-    assert host.run("go version").stdout.startswith('go version go1.15')
+    assert host.exists("/usr/local/opt/go/libexec/bin/go")
+    assert host.run("/usr/local/opt/go/libexec/bin/go version").stdout.startswith('go version go1.15')
 
 def test_go_src_exists(host):
     folder_base_path = "/home/linuxbrew/go/"
