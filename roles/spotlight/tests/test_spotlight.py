dc6ee116c41fec30efd4637784e9cc48cb2988f7
fehersanyi-bitrise <sandor.feher@bitrise.io>
2021-03-19 13:32:00 +0100

    remove tests for now

--- roles/spotlight/tests/test_spotlight.py
+++
@@ -1,7 +0,0 @@
-import unittest
-import os
-import testinfra.utils.ansible_runner
-
-class TestBaseOS(unittest.TestCase):
-    def test_spotlight_is_disabled(self):
-        self.assertEqual(self.host.run("sudo launchctl list | grep -q com.apple.metadata.mds").rc, 0)
