104c07d14e78f9d9ff814886d6c0ad5e7e7d1879
Marton Szasz <marton.szasz@bitrise.io>
2021-05-19 18:40:50 +0200

    [IN-2675] Flutter to FVM transition

---
+++ roles/flutter/files/flutter.sh
@@ -0,0 +1,4 @@
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+# export PATH="$PATH":"$HOME/fvm/default/bin"
+export PATH="$PATH":"$(brew --prefix)/bin"
+export PATH="$PATH":"/home/linuxbrew/.linuxbrew/bin"
